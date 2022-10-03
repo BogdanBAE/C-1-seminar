083d3ab9157f588773d81645039e1ac7480a260f
BogdanBAE <bogdan.elistratov@gmail.com>
2022-10-03 19:54:38 +0400

    Добавил первый коммит

---
+++ задача 1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.Write(" ВВедите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+if (firstNumber == Convert.ToInt32(Math.Pow(secondNumber, 2)))
+{
+    Console.Write($"Первое число {firstNumber} является квадратом второго числа {secondNumber}");
+} else
+{
+    Console.Write($"Первое число {firstNumber} НЕ является квадратом второго числа {secondNumber}");
+}
