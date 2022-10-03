937f009b246ee3448e55cde6e10554bb605989ca
BogdanBAE <bogdan.elistratov@gmail.com>
2022-10-03 21:35:33 +0400

    Сделал еще пару задач

---
+++ задача 5/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int number = Math.Abs(N);
+
+for (int i = number*(-1); i<= number; i++) {
+    Console.Write($"{i} ");
+}
---
+++ задача 7/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int N = Math.Abs(int.Parse(Console.ReadLine()));
+
+if (N<100|| N> 999) {
+    Console.Write("Введено не трехзначное число");
+    return;
+}
+
+int division = N % 10;
+Console.WriteLine($"{division} последняя цифра числа {N}");
